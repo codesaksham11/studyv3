6a02c94b210cb6a5ef3feec4702efb204c9822b7
codesaksham11 <bhattaraij51@gmail.com>
2025-03-14 20:03:55 +0545

    Create atomic_structure.js

---
+++ data/chemistry/atomic_structure.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        front: "What are the three subatomic particles?",
+        back: "Protons, Neutrons, and Electrons"
+    },
+    {
+        front: "What is the atomic number?",
+        back: "The number of protons in an atom's nucleus"
+    },
+    {
+        front: "image of an atom",
+        back: "<img src='../images/IMG_20250311_193606.jpg' alt='atom'>"
+    }
+];
