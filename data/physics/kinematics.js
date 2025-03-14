064a6ec88ede67c7971f4ab78e54cdc040fa1958
codesaksham11 <bhattaraij51@gmail.com>
2025-03-14 20:05:14 +0545

    Create kinematics.js

---
+++ data/physics/kinematics.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        front: "What is displacement?",
+        back: "Change in position of an object."
+    },
+    {
+        front: "Define velocity.",
+        back: "The rate of change of displacement with respect to time."
+    }
+];
